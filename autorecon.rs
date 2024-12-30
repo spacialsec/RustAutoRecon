c11d06705f44db80de393c7083122f45243bf593
spacialsec <sales@spatial-sec.com>
2024-12-30 07:53:41 -0700

    Create autorecon.rs

---
+++ autorecon.rs
@@ -0,0 +1,16 @@
+use std::env;
+use std::process::Command;
+
+fn main() {
+   let args: Vec<String> = env::args().collect();
+
+   let output = Command::new("nice")
+       .args(&["-n"])
+       .args(&["20"]) // Make Blazing Fast
+       .args(&["autorecon"])
+       .args(&args[1..])
+       .output()
+       .expect("Failed to execute command");
+
+   println!("{}", String::from_utf8_lossy(&output.stdout));
+}
